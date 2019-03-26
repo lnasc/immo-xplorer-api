711c4f6a19fcd8f7f57267cc1f3e592464760ba2
ludovic nascimbeni <ludovic.nascimbeni@hotmail.fr>
2019-03-26 23:36:05 +0100

    Renamed entrypoint to server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+"use strict";
+console.log("Running webapp...");
+
+let express = require('express');
+let bodyParser = require('body-parser');
+var cors = require('cors');
+
+let app = new express();
+let port = process.env.PORT || 4243;
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('./app'));
+
+app.get('/api/ads',(req,res)=>{
+    let adsController = require('./controllers/adsController');
+    adsController.getAds().subscribe(result => {
+        res.status(200).json(result);
+    });
+})
+app.listen(port,()=>{
+	//console.log(`Running webhook listener...`);
+	console.log(`App listening on port ${port}`);
+}) ;
